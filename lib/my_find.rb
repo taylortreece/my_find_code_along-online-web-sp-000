32b50b74d494fffbf770906df352813110b24ada
Taylor Treece <tayloratreece@gmail.com>
2020-12-31 20:48:36 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,21 @@
 require 'pry'
 
 def my_find(collection)
-
+  i = 0
+  while i < collection.length
+  if yield(collection[i])
+    return collection[i]
+  end
+  i += 1
 end
+end
+
+array = [
+  "hailey",
+  "taylor",
+  "kelsey"
+]
+
+array[]
+
+my_find(array)
